7d1cd0cc5009490828e9bc0db3cef255ceeaafbd
Nate Yonkee <tulanthoar@gmail.com>
2016-04-24 12:59:24 -0600

    get the right files

--- practice/src/copypractice.py
+++
@@ -1,8 +0,0 @@
-import os
-myf = input( "enter name " )
-print( myf )
-f = open( myf )
-newf = open( "copyf", 'w' )
-newf.write( f.read() )
-f.close()
-newf.close()
--- practice/src/mturtle.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-import turtle
-
-
-print( dir( turtle ) )
-p = turtle.Pen()
-sleep( 0.2 )
-p.forward( 5 )
-
